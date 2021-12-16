d5ce9e2edfa34d82f06927a7904f8c97a7cc0eca
luisvillanuevaf <lvillanuevaf@unsa.edu.pe>
2021-12-16 08:57:31 -0500

    add questions

--- src/Components/Quiz.js
+++ src/Components/Quiz.js
@@ -8,80 +8,181 @@ class Quiz extends Component {
   json = {
     title: "Carbon Footprint UNSA",
     pages: [
-      {
-        title: "What operating system do you use?",
-        elements: [
-          {
-            type: "checkbox",
-            name: "opSystem",
-            title: "OS",
-            hasOther: true,
-            isRequired: true,
-            choices: ["Windows", "Linux", "Macintosh OSX"],
-          },
-        ],
-      },
-      {
-        title: "What language(s) are you currently using?",
-        elements: [
-          {
-            type: "checkbox",
-            name: "langs",
-            title: "Please select from the list",
-            colCount: 4,
-            isRequired: true,
-            choices: [
-              "Javascript",
-              "Java",
-              "Python",
-              "CSS",
-              "PHP",
-              "Ruby",
-              "C++",
-              "C",
-              "Shell",
-              "C#",
-              "Objective-C",
-              "R",
-              "VimL",
-              "Go",
-              "Perl",
-              "CoffeeScript",
-              "TeX",
-              "Swift",
-              "Scala",
-              "Emacs Lisp",
-              "Haskell",
-              "Lua",
-              "Clojure",
-              "Matlab",
-              "Arduino",
-              "Makefile",
-              "Groovy",
-              "Puppet",
-              "Rust",
-              "PowerShell",
-            ],
-          },
-        ],
-      },
-      {
-        title: "Please enter your name and e-mail",
-        elements: [
-          {
-            type: "text",
-            name: "name",
-            title: "Name:",
-          },
-          {
-            type: "text",
-            name: "email",
-            title: "Your e-mail",
-          },
-        ],
-      },
-    ],
-  };
+        {
+            "title": "Hogar",
+            "elements": [
+                {
+                    "type": "rating",
+                    "name": "numberHome",
+                    "title": "¿Cuántas personas viven en tu hogar?",
+                    "hasOther": true,
+                    "isRequired": true,
+                    "rateMin": 0,
+                    "rateMax": 10,
+                    "minRateDescription": "(Cantidad mínima)",
+                    "maxRateDescription": "(Cantidad máxima)",
+                }
+            ]
+        },
+        {
+            "title": "Transporte",
+            "elements": [
+                {
+                    "type": "text",
+                    "name": "transport1",
+                    "title": "Si tiene auto privado, normalmente con cuantas personas lo comparte?",
+                    "isRequired": false,
+                    "validators": [
+                        {
+                            "type": "numeric",
+                            "minValue": 2,
+                            "maxValue": 10000
+                        }
+                    ]
+                },
+
+                {
+                    "type": "text",
+                    "name": "transport2",
+                    "title": "Si tiene auto privado, ¿Cuántas horas a la semana lo utiliza? ",
+                    "hasOther": true,
+                    "isRequired": false,
+                    "validators": [
+                        {
+                            "type": "numeric",
+                            "minValue": 5,
+                            "maxValue": 10000
+                        }
+                    ]
+                },
+                {
+                    "type": "boolean",
+                    "name": "transport3",
+                    "title": "¿Usa transporte público? ",
+                    "hasOther": true,
+                    "isRequired": true,
+
+                },
+                {
+                    "type": "text",
+                    "name": "transport4",
+                    "visibleIf": "{transport3} == true",
+                    "title": "¿Cuántas horas pasa a la semana en promedio en el transporte público? ",
+                    "isRequired": true,
+                    "validators": [
+                        {
+                            "type": "numeric",
+                            "minValue": 3,
+                            "maxValue": 10000
+                        }
+                    ]
+
+                },
+                {
+                    "type": "text",
+                    "name": "transport5",
+                    "visibleIf": "{transport3} == true",
+                    "title": "¿Cuánto dinero gasta en gasolina por semana?",
+                    "isRequired": true,
+                    "validators": [
+                        {
+                            "type": "numeric",
+                            "minValue": 0,
+                            "maxValue": 10000
+                        }
+                    ]
+
+                },
+                {
+                    "type": "text",
+                    "name": "transport6",
+                    "visibleIf": "{transport3} == true",
+                    "title": "¿Cantidad de horas que viaja al año en avión?",
+                    "isRequired": true,
+                    "validators": [
+                        {
+                            "type": "numeric",
+                            "minValue": 0,
+                            "maxValue": 10000
+                        }
+                    ]
+
+                },
+            ]
+        },
+        {
+            "title": "Energía",
+            "elements": [
+                {
+                    "type": "text",
+                    "name": "energy1",
+                    "title": "¿Cuánto pagas por recibo de luz al mes? ",
+                    "isRequired": true,
+                    "validators": [
+                        {
+                            "type": "numeric",
+                            "minValue": 10,
+                            "maxValue": 10000
+                        }
+                    ]
+
+                },
+                {
+                    "type": "text",
+                    "name": "energy2",
+                    "title": "¿Cuántos balones de gas de 10kg utilizas en tu casa al mes?",
+                    "isRequired": true,
+                    "validators": [
+                        {
+                            "type": "numeric",
+                            "minValue": 1,
+                            "maxValue": 10000
+                        }
+                    ]
+
+                },
+            ]
+        },
+        {
+            "title": "Alimentación",
+            "elements": [
+                {
+                    "type": "text",
+                    "name": "aliment1",
+                    "title": "¿Cuántas veces comes carne a la semana?",
+                    "isRequired": true,
+                    "validators": [
+                        {
+                            "type": "numeric",
+                            "minValue": 0,
+                            "maxValue": 10000
+                        }
+                    ]
+
+                },
+            ]
+        },
+        {
+            "title": "Residuos",
+            "elements": [
+                {
+                    "type": "text",
+                    "name": "residue1",
+                    "title": "¿Cuántas bolsas de plástico utilizas solamente tú a la semana?",
+                    "isRequired": true,
+                    "validators": [
+                        {
+                            "type": "numeric",
+                            "minValue": 0,
+                            "maxValue": 10000
+                        }
+                    ]
+
+                },
+            ]
+        }
+    ]
+};
 
   render() {
     var model = new Survey.Model(this.json);
