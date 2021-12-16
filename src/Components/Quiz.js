5a92a854967f0e6e33eb79d1e9e853c33de4a6db
Andy <andy@apurata.com>
2021-12-16 12:59:57 -0500

    Add images

--- src/Components/Quiz.js
+++ src/Components/Quiz.js
@@ -3,16 +3,26 @@ import "survey-react/survey.css";
 import React, { Component } from "react";
 
 Survey.StylesManager.applyTheme("stone");
-Survey.surveyLocalization.locales[Survey.surveyLocalization.defaultLocale].requiredError = "Este campo no puede ir vacío";
+Survey.surveyLocalization.locales[
+  Survey.surveyLocalization.defaultLocale
+].requiredError = "Este campo no puede ir vacío";
 class Quiz extends Component {
   //Define Survey JSON
   //Here is the simplest Survey with one text question
   json = {
-    title: "Carbon Footprint UNSA",
+    title: "Calcula tu huella de carbono de manera sencilla",
     pages: [
       {
-        title: "Datos personales",
+        name: "Datos personales",
+        navigationTitle: "Datos personales",
+        navigationDescription: "Datos personales",
         elements: [
+          {
+            type: "image",
+            name: "first_page_image",
+            imageLink: "/images/personal.png",
+            imageHeight: 200,
+          },
           {
             type: "text",
             name: "name",
@@ -29,179 +39,219 @@ class Quiz extends Component {
           },
         ],
       },
-      // {
-      //   title: "Hogar",
-      //   elements: [
-      //     {
-      //       type: "rating",
-      //       name: "numberHome",
-      //       title: "¿Cuántas personas viven en tu hogar?",
-      //       hasOther: true,
-      //       isRequired: true,
-      //       rateMin: 0,
-      //       rateMax: 10,
-      //       minRateDescription: "(Cantidad mínima)",
-      //       maxRateDescription: "(Cantidad máxima)",
-      //     },
-      //   ],
-      // },
-      // {
-      //   title: "Transporte",
-      //   elements: [
-      //     {
-      //       type: "text",
-      //       name: "transport1",
-      //       title:
-      //         "Si tiene auto privado, normalmente con cuantas personas lo comparte?",
-      //       isRequired: false,
-      //       validators: [
-      //         {
-      //           type: "numeric",
-      //           minValue: 2,
-      //           maxValue: 10000,
-      //         },
-      //       ],
-      //     },
-
-      //     {
-      //       type: "text",
-      //       name: "transport2",
-      //       title:
-      //         "Si tiene auto privado, ¿Cuántas horas a la semana lo utiliza? ",
-      //       hasOther: true,
-      //       isRequired: false,
-      //       validators: [
-      //         {
-      //           type: "numeric",
-      //           minValue: 5,
-      //           maxValue: 10000,
-      //         },
-      //       ],
-      //     },
-      //     {
-      //       type: "boolean",
-      //       name: "transport3",
-      //       title: "¿Usa transporte público? ",
-      //       hasOther: true,
-      //       isRequired: true,
-      //     },
-      //     {
-      //       type: "text",
-      //       name: "transport4",
-      //       visibleIf: "{transport3} == true",
-      //       title:
-      //         "¿Cuántas horas pasa a la semana en promedio en el transporte público? ",
-      //       isRequired: true,
-      //       validators: [
-      //         {
-      //           type: "numeric",
-      //           minValue: 3,
-      //           maxValue: 10000,
-      //         },
-      //       ],
-      //     },
-      //     {
-      //       type: "text",
-      //       name: "transport5",
-      //       visibleIf: "{transport3} == true",
-      //       title: "¿Cuánto dinero gasta en gasolina por semana?",
-      //       isRequired: true,
-      //       validators: [
-      //         {
-      //           type: "numeric",
-      //           minValue: 0,
-      //           maxValue: 10000,
-      //         },
-      //       ],
-      //     },
-      //     {
-      //       type: "text",
-      //       name: "transport6",
-      //       visibleIf: "{transport3} == true",
-      //       title: "¿Cantidad de horas que viaja al año en avión?",
-      //       isRequired: true,
-      //       validators: [
-      //         {
-      //           type: "numeric",
-      //           minValue: 0,
-      //           maxValue: 10000,
-      //         },
-      //       ],
-      //     },
-      //   ],
-      // },
-      // {
-      //   title: "Energía",
-      //   elements: [
-      //     {
-      //       type: "text",
-      //       name: "energy1",
-      //       title: "¿Cuánto pagas por recibo de luz al mes? ",
-      //       isRequired: true,
-      //       validators: [
-      //         {
-      //           type: "numeric",
-      //           minValue: 10,
-      //           maxValue: 10000,
-      //         },
-      //       ],
-      //     },
-      //     {
-      //       type: "text",
-      //       name: "energy2",
-      //       title:
-      //         "¿Cuántos balones de gas de 10kg utilizas en tu casa al mes?",
-      //       isRequired: true,
-      //       validators: [
-      //         {
-      //           type: "numeric",
-      //           minValue: 1,
-      //           maxValue: 10000,
-      //         },
-      //       ],
-      //     },
-      //   ],
-      // },
-      // {
-      //   title: "Alimentación",
-      //   elements: [
-      //     {
-      //       type: "text",
-      //       name: "aliment1",
-      //       title: "¿Cuántas veces comes carne a la semana?",
-      //       isRequired: true,
-      //       validators: [
-      //         {
-      //           type: "numeric",
-      //           minValue: 0,
-      //           maxValue: 10000,
-      //         },
-      //       ],
-      //     },
-      //   ],
-      // },
-      // {
-      //   title: "Residuos",
-      //   elements: [
-      //     {
-      //       type: "text",
-      //       name: "residue1",
-      //       title:
-      //         "¿Cuántas bolsas de plástico utilizas solamente tú a la semana?",
-      //       isRequired: true,
-      //       validators: [
-      //         {
-      //           type: "numeric",
-      //           minValue: 0,
-      //           maxValue: 10000,
-      //         },
-      //       ],
-      //     },
-      //   ],
-      // },
+      {
+        name: "Hogar",
+        navigationTitle: "Hogar",
+        navigationDescription: "Hogar",
+        elements: [
+          {
+            type: "image",
+            name: "first_page_image",
+            imageLink: "/images/house.svg",
+            imageHeight: 200,
+          },
+          {
+            type: "rating",
+            name: "numberHome",
+            title: "¿Cuántas personas viven en tu hogar?",
+            hasOther: true,
+            isRequired: true,
+            rateMin: 0,
+            rateMax: 10,
+            minRateDescription: "(Cantidad mínima)",
+            maxRateDescription: "(Cantidad máxima)",
+          },
+        ],
+      },
+      {
+        name: "Transporte",
+        navigationTitle: "Transporte",
+        navigationDescription: "Transporte",
+        elements: [
+          {
+            type: "image",
+            name: "first_page_image",
+            imageLink: "/images/car.svg",
+            imageHeight: 200,
+          },
+          {
+            type: "text",
+            name: "transport1",
+            title:
+              "Si tiene auto privado, normalmente con cuantas personas lo comparte?",
+            isRequired: false,
+            validators: [
+              {
+                type: "numeric",
+                minValue: 2,
+                maxValue: 10000,
+              },
+            ],
+          },
+          {
+            type: "text",
+            name: "transport2",
+            title:
+              "Si tiene auto privado, ¿Cuántas horas a la semana lo utiliza? ",
+            hasOther: true,
+            isRequired: false,
+            validators: [
+              {
+                type: "numeric",
+                minValue: 5,
+                maxValue: 10000,
+              },
+            ],
+          },
+          {
+            type: "boolean",
+            name: "transport3",
+            title: "¿Usa transporte público? ",
+            hasOther: true,
+            isRequired: true,
+          },
+          {
+            type: "text",
+            name: "transport4",
+            visibleIf: "{transport3} == true",
+            title:
+              "¿Cuántas horas pasa a la semana en promedio en el transporte público? ",
+            isRequired: true,
+            validators: [
+              {
+                type: "numeric",
+                minValue: 3,
+                maxValue: 10000,
+              },
+            ],
+          },
+          {
+            type: "text",
+            name: "transport5",
+            visibleIf: "{transport3} == true",
+            title: "¿Cuánto dinero gasta en gasolina por semana?",
+            isRequired: true,
+            validators: [
+              {
+                type: "numeric",
+                minValue: 0,
+                maxValue: 10000,
+              },
+            ],
+          },
+          {
+            type: "text",
+            name: "transport6",
+            visibleIf: "{transport3} == true",
+            title: "¿Cantidad de horas que viaja al año en avión?",
+            isRequired: true,
+            validators: [
+              {
+                type: "numeric",
+                minValue: 0,
+                maxValue: 10000,
+              },
+            ],
+          },
+        ],
+      },
+      {
+        name: "Energía",
+        navigationTitle: "Energía",
+        navigationDescription: "Energía",
+        elements: [
+          {
+            type: "image",
+            name: "first_page_image",
+            imageLink: "/images/energia.svg",
+            imageHeight: 200,
+          },
+          {
+            type: "text",
+            name: "energy1",
+            title: "¿Cuánto pagas por recibo de luz al mes? ",
+            isRequired: true,
+            validators: [
+              {
+                type: "numeric",
+                minValue: 10,
+                maxValue: 10000,
+              },
+            ],
+          },
+          {
+            type: "text",
+            name: "energy2",
+            title:
+              "¿Cuántos balones de gas de 10kg utilizas en tu casa al mes?",
+            isRequired: true,
+            validators: [
+              {
+                type: "numeric",
+                minValue: 1,
+                maxValue: 10000,
+              },
+            ],
+          },
+        ],
+      },
+      {
+        name: "Alimentación",
+        navigationTitle: "Alimentación",
+        navigationDescription: "Alimentación",
+        elements: [
+          {
+            type: "image",
+            name: "first_page_image",
+            imageLink: "/images/food.svg",
+            imageHeight: 200,
+          },
+          {
+            type: "text",
+            name: "aliment1",
+            title: "¿Cuántas veces comes carne a la semana?",
+            isRequired: true,
+            validators: [
+              {
+                type: "numeric",
+                minValue: 0,
+                maxValue: 10000,
+              },
+            ],
+          },
+        ],
+      },
+      {
+        name: "Residuos",
+        navigationTitle: "Residuos",
+        navigationDescription: "Residuos",
+        elements: [
+          {
+            type: "image",
+            name: "first_page_image",
+            imageLink: "/images/residuos.svg",
+            imageHeight: 200,
+          },
+          {
+            type: "text",
+            name: "residue1",
+            title:
+              "¿Cuántas bolsas de plástico utilizas solamente tú a la semana?",
+            isRequired: true,
+            validators: [
+              {
+                type: "numeric",
+                minValue: 0,
+                maxValue: 10000,
+              },
+            ],
+          },
+        ],
+      },
     ],
+    showProgressBar: "top",
+    progressBarType: "buttons",
   };
-
   render() {
     var model = new Survey.Model(this.json);
     model.onComplete.add(function (sender) {
@@ -234,6 +284,7 @@ class Quiz extends Component {
       data.email = email;
       data.name = name;
       data.result = HC;
+      console.log(data);
       fetch("http://54.149.211.70:5000/save-data", {
         method: "POST",
         body: JSON.stringify(data),
@@ -249,7 +300,6 @@ class Quiz extends Component {
         );
       console.log(data);
     });
-    model.showProgressBar = "bottom";
     return <Survey.Survey model={model} />;
   }
 }
