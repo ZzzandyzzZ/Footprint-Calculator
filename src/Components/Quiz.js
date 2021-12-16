583c427f967f2514bd3a145708af2729f4ec0aad
Eduardo Sanchez <esanchez.h25@gmail.com>
2021-12-16 13:28:19 -0500

    post

--- src/Components/Quiz.js
+++ src/Components/Quiz.js
@@ -310,6 +310,7 @@ class Quiz extends Component {
       data.name = name;
       data.result = HC;
       console.log(data);
+      model.setVariable("resultado", HC.toFixed(2));
       fetch("http://54.211.176.115:5000/save-data", {
         method: "POST",
         body: JSON.stringify(data),
